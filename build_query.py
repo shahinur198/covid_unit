a9210a0f2028a4157b4b85d960d9bdefdaa7053a
Shahinur Rahman <34091626+shahinur198@users.noreply.github.com>
2023-01-03 20:50:00 +0600

    Update build_query.py

--- build_query.py
+++ build_query.py
@@ -1,4 +1,4 @@
-def insert_query(table,names,values):
+def insert_query(table,names,values,id):
 
 	if len(names)!=len(values):
 		return None
@@ -24,6 +24,7 @@ def insert_query(table,names,values):
 			qur = qur +"'"+value+"') "
 
 		i=i+1
+	qur=qur+"RETURNING "+id
 
 	return qur
 
@@ -63,4 +64,40 @@ def select_query(table,column,where_stetment):
 	
 	return qur
 
+def insert_multiple_query(table,names,values,id):
+
+	# if len(names)!=len(values):
+	# 	return None
+
+	qur= " INSERT INTO "+table+"("
+	i=0;
+	ln = len(names)-1
+	for name in names:
+		if i<ln:
+			qur = qur +name+","
+		else:
+			qur = qur +name+")"
+
+		i=i+1
+
+	qur= qur+" VALUES"
+	j=0
+	for values_tmp in values:
+		qur= qur+"("
+		i=0;
+		ln = len(values_tmp)-1
+		for value in values_tmp:
+			if i<ln:
+				qur = qur +"'"+value+"',"
+			else:
+				qur = qur +"'"+value+"') "
+				if j<len(values)-1:
+					qur = qur +", "
+
+			i=i+1
+		j=j+1
+
+	qur=qur+"RETURNING "+id
+
+	return qur
 	
